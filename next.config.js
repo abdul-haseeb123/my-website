614f1342e32bf7ea5c33f74c0124ae90b484a9b5
abdul-haseeb123 <syedabdulhaseeb611@gmail.com>
2024-02-16 18:04:04 +0500

    modifies the next.config.js and out folder

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: "/my-website",
   output: "export",
   reactStrictMode: true,
 };
---
+++ out/_next/static/chunks/app/lessons/[slug]/page-5f54f81ef93706ae.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[697,688,301],{1587:function(){}},function(n){n.O(0,[120,543,744],function(){return n(n.s=1587)}),_N_E=n.O()}]);
